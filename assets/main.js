4e369afd1fd985bc3d34bc053c8b5992ccf778d1
rhetesi <rhetesi@gmail.com>
2021-01-03 00:15:11 +0100

    Add: hungarian flag CSS

--- assets/main.js
+++ assets/main.js
@@ -1,2 +1,8 @@
 'use strict';
 
+/*
+Amikor az alkalmazás betöltődik indíts el egyidejűleg kettő fetch kérést az alábbi két url-re, Promise.all használatával:
+https://raw.githubusercontent.com/jokecamp/FootballData/master/UEFA_European_Championship/Euro%202016/players_json/teams.json
+https://raw.githubusercontent.com/jokecamp/FootballData/master/UEFA_European_Championship/Euro%202016/players_json/hungary-players.json
+*/
+
